e85f06ea4a0c41860177fa3415f23bdab553a6b4
Isaac Asensio <isaac.asensio@gmail.com>
2016-09-22 22:23:17 +0100

    Add sdkman to bash_profile

--- jdk.sh
+++ jdk.sh
@@ -6,6 +6,12 @@ brew cask install --appdir="~/Applications" java
 curl -s "https://get.sdkman.io" | bash
 
 # Activate
+if grep -q "sdkman" ~/.bash_profile; then
+  echo "sdkman already added to bash_profile"
+else
+  echo "source ~/.sdkman/bin/sdkman-init.sh" >> ~/.bash_profile
+fi
+
 source ~/.sdkman/bin/sdkman-init.sh
 
 sed -i -e 's/sdkman_auto_answer=false/sdkman_auto_answer=true/g' ~/.sdkman/etc/config
