2d0a2f621fd80c89dc082040f03f27bb00ae10f6
Isaac Asensio <isaac.asensio@gmail.com>
2024-12-11 23:21:24 +0100

    update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -14,7 +14,18 @@ config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
-config.default_prog = { '/usr/local/bin/zellij', '-l', 'welcome' }
+
+-- Then you'll be able to set your terminal to `xterm-kitty` as seen below
+config.term = "xterm-kitty"
+
+-- To enable kitty graphics
+-- https://github.com/wez/wezterm/issues/986
+-- It seems that kitty graphics is not enabled by default according to this
+-- Not sure, so I'm enabling it just in case
+-- https://github.com/wez/wezterm/issues/1406#issuecomment-996253377
+config.enable_kitty_graphics = true
+
+-- config.default_prog = { '/usr/local/bin/zellij', '-l', 'welcome' }
 
 config.color_scheme = "Catppuccin Macchiato"
 
