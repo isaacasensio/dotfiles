f27d9d37d2448fb5e3fda797453641f630d1ed9c
Isaac Asensio <isaac.asensio@gmail.com>
2025-02-25 22:36:08 +0100

    replace sketchbybar with simple-bar

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Array of names to stow
-names=("bat" "btop" "git" "mc" "zsh" "sketchybar" "oh-my-posh" "wallpapers" "yabai" "ghostty" "karabiner" "tmux" "nvim" "yazi" "sesh")
+names=("bat" "btop" "git" "mc" "zsh" "ubersicht" "oh-my-posh" "wallpapers" "yabai" "ghostty" "karabiner" "tmux" "nvim" "yazi" "sesh")
 
 for name in "${names[@]}"; do
     echo "Running: stow --target $HOME $name"
@@ -28,11 +28,10 @@ echo "Installing the wallpaper"
 m wallpaper $HOME/.config/wallpapers/nice-blue-background.png
 echo "Successfully installed the wallpaper"
 
-echo "Reloading sketchybar"
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.23/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
-brew services restart felixkratz/formulae/sketchybar
-sketchybar --reload
-echo "Successfully reloaded sketchybar"
+echo "Install simple-bar"
+git clone git@github.com:Jean-Tinland/simple-bar.git  $HOME/.config/ubersicht/widgets/simple-bar
+osascript -e 'tell application id "tracesOf.Uebersicht" to refresh'
+echo "Successfully reloaded ubersicht"
 
 
 echo "All commands completed successfully."
